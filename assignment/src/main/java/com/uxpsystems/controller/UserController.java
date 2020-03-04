f5d68858ba44cc6101fb3ff933bb15c32f5cd158
SandipsingRathod <43872248+SandipsingRathod@users.noreply.github.com>
2020-03-04 16:15:37 +0530

    added logger and removed commented code

--- assignment/src/main/java/com/uxpsystems/controller/UserController.java
+++ assignment/src/main/java/com/uxpsystems/controller/UserController.java
@@ -3,9 +3,7 @@ package com.uxpsystems.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import javax.validation.Valid;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,22 +41,6 @@ public class UserController {
 		return "Hello sandy";
 	}
 
-	/*
-	 * @GetMapping("/user") public List<User> getLoginUser() {
-	 * 
-	 * Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-	 * 
-	 * Object credentials = auth.getCredentials(); Collection<? extends
-	 * GrantedAuthority> authorities = auth.getAuthorities(); Object details =
-	 * auth.getDetails(); String name = auth.getName(); Object principal =
-	 * auth.getPrincipal(); boolean authenticated = auth.isAuthenticated();
-	 * 
-	 * log.info(credentials +" "+
-	 * authorities+" "+details+" "+name+" "+principal+" "+authenticated);
-	 * 
-	 * return userRepository.findByUsername(name).get(); }
-	 */
-
 	@GetMapping("/allusers")
 	@PreAuthorize("hasRole('ADMIN')")
 	public List<User> getAllEmployees() {
@@ -67,65 +49,54 @@ public class UserController {
 
 	@GetMapping("/users/{id}")
 	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
-	public ResponseEntity<User> getEmployeeById(@PathVariable(value = "id") Long employeeId)
+	public ResponseEntity<User> getEmployeeById(@PathVariable(value = "id") Long userId)
 			throws ResourceNotFoundException {
-		User employee = userRepository.findById(employeeId)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
-		return ResponseEntity.ok().body(employee);
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + userId));
+		return ResponseEntity.ok().body(user);
 	}
 
 	@PostMapping("/saveusers")
 	@PreAuthorize("hasRole('ADMIN')")
-	public User createEmployee(@Valid @RequestBody User employee) {
+	public User createEmployee(@Valid @RequestBody User user) {
 
-		String encoded = new BCryptPasswordEncoder().encode("admin@123");
-		employee.setPassword(encoded);
-		return userRepository.save(employee);
+		String encoded = new BCryptPasswordEncoder().encode(user.getPassword());
+		user.setPassword(encoded);
+
+		log.info("saving user with username " + user.getUsername());
+		return userRepository.save(user);
 	}
 
 	@PutMapping("/updateuser/{id}")
 	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
-	public ResponseEntity<User> updateEmployee(@PathVariable(value = "id") Long employeeId,
-			@Valid @RequestBody User employeeDetails) throws ResourceNotFoundException {
-		User employee = userRepository.findById(employeeId)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+	public ResponseEntity<User> updateEmployee(@PathVariable(value = "id") Long userId,
+			@Valid @RequestBody User userDetails) throws ResourceNotFoundException {
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + userId));
 
-		employee.setUsername(employeeDetails.getUsername());
-		employee.setPassword(employeeDetails.getPassword());
-		employee.setEmail(employeeDetails.getEmail());
-		employee.setStatus(employeeDetails.getStatus());
+		user.setUsername(userDetails.getUsername());
+		user.setPassword(userDetails.getPassword());
+		user.setEmail(userDetails.getEmail());
+		user.setStatus(userDetails.getStatus());
 
-		final User updatedEmployee = userRepository.save(employee);
-		return ResponseEntity.ok(updatedEmployee);
+		final User updatedUser = userRepository.save(user);
+		log.info("updating user with username " + user.getUsername());
+		return ResponseEntity.ok(updatedUser);
 	}
 
 	@DeleteMapping("/deleteuser/{id}")
 	@PreAuthorize("hasRole('ADMIN')")
-	public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
+	public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long userId)
 			throws ResourceNotFoundException {
-		User employee = userRepository.findById(employeeId)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+		User employee = userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + userId));
 
 		userRepository.delete(employee);
 		Map<String, Boolean> response = new HashMap<>();
 		response.put("deleted", Boolean.TRUE);
+		log.info("deleting user having user Id " + userId);
 		return response;
 	}
 
-	/*
-	 * @GetMapping("/api/test/user")
-	 * 
-	 * @PreAuthorize("hasRole('USER') or hasRole('ADMIN')") public String
-	 * userAccess() { return ">>> User Contents!"; }
-	 * 
-	 * @GetMapping("/api/test/pm")
-	 * 
-	 * @PreAuthorize("hasRole('PM')") public String projectManagementAccess() {
-	 * return ">>> Board Management Project"; }
-	 * 
-	 * @GetMapping("/api/test/admin")
-	 * 
-	 * @PreAuthorize("hasRole('ADMIN')") public String adminAccess() { return
-	 * ">>> Admin Contents"; }
-	 */
 }
+
